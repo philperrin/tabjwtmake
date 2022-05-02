bbcf7c0f10f922e4f80f3b24c15efba7ac656337
Phil Perrin <pperrin@phdata.io>
2022-05-02 07:21:23 -0600

    summary

--- tabjwtmake.py
+++ tabjwtmake.py
@@ -1,23 +1,23 @@
 import jwt
 import datetime
 import uuid
-import os
+# import os
 
 def mktoken():
     token = jwt.encode(
 	{
-		"iss": os.getenv('ClientID'), #ClientID
+		"iss": '159802a9-bc87-46df-9635-93b75a01a631', #ClientID
 		"exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
 		"jti": str(uuid.uuid4()),
 		"aud": "tableau",
-		"sub": os.getenv('UserID'), #User
+		"sub": 'pperrin@phdata.io', #User
 		"scp": ["tableau:views:embed", "tableau:metrics:embed"]
 	},
-		os.getenv('SecretVal'), #SecretVal
+		'VdzA3pZ8Dek91znHR7HQWocCSo6FUQ9rF8fhpK2L+ig=', #SecretVal
 		algorithm = "HS256",
 		headers = {
-		'kid': os.getenv('SecretID'), #SecretID
-		'iss': os.getenv('ClientID') #ClientID
+		'kid': '47cd7668-0308-4652-91d6-e42c13b5bbb4', #SecretID
+		'iss': '159802a9-bc87-46df-9635-93b75a01a631' #ClientID
         }
   )
     return token
